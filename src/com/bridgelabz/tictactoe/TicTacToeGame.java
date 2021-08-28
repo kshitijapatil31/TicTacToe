5e69d2a64770f5ab5dcd7847f1d6b94d2555a164
Kshitija Patil <patilkshitija2@gmail.com>
2021-08-28 11:59:51 +0530

    [kshitija]: created a method to allow player to input letter

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,17 +1,37 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	String turn1="x";
+	String turn2;
 	public  void createBoard() {
+		
 		char[] board = new char[10];
-      String emptyBoard=board.toString();
+      
 		for(int i=0;i<10;i++) {
 			board[i]=' ';
 		}
 	}
 
+	public void allowPlayer() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the letter X or O");
+		String letter=sc.next();
+		
+		if(turn1.equals(letter)) {
+			turn2="o";
+		}else {
+			turn2="x";
+		}
+		System.out.println(turn1);
+		System.out.println(turn2);
+	
+	}
 	public static void main(String[] args) {
 		TicTacToeGame tictactoe=new TicTacToeGame();
 		tictactoe.createBoard();
+		tictactoe.allowPlayer();
 	}
 
 }
